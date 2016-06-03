216508b4f0d71518e2dfb836451910ee1bafeecf
Eugene Sia <eugene.sia@ticketmaster.co.uk>
2016-06-03 03:42:30 +0100

    Program an invisible UIController sect 6.3.1

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+// Import UI code framework.
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+	// Reference text object in scene to set text property.
+	[SerializeField] private Text scoreLabel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreLabel.text = Time.realtimeSinceStartup.ToString();
+	}
+
+	// Method called by settings button.
+	public void OnOpenSettings() {
+		Debug.Log("open settings");
+	}
+
+	// Method called by Event Trigger on settings button.
+	public void OnPointerDown() {
+		Debug.Log("pointer down");
+	}
+}
