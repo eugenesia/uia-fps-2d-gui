d00b70f48808fb8c4fd571fca5c210acacd338c3
Eugene Sia <eugene.sia@ticketmaster.co.uk>
2016-06-05 12:44:26 +0100

    Set values using sliders and input fields sect 6.3.3

--- Assets/Scripts/SettingsPopup.cs
+++ Assets/Scripts/SettingsPopup.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SettingsPopup : MonoBehaviour {
 
+	// Slider object to choose the speed.
+	[SerializeField] private Slider speedSlider;
+
 	// Use this for initialization
 	void Start () {
-	
+		// Get speed value from saved storage.
+		speedSlider.value = PlayerPrefs.GetFloat("speed", 1);
 	}
 	
 	// Update is called once per frame
@@ -21,5 +26,17 @@ public class SettingsPopup : MonoBehaviour {
 		// When settings popup is closed, hide everything.
 		public void Close() {
 		gameObject.SetActive(false);
+	}
+
+	// Will trigger when user types in the input field.
+	public void OnSubmitName(string name) {
+		Debug.Log(name);
+	}
+
+	// Will trigger when user adjusts slider.
+	public void OnSpeedValue(float speed) {
+		Debug.Log("Speed: " + speed);
+		// Save the speed value, to reload on start.
+		PlayerPrefs.SetFloat("speed", speed);
 	}
 }
