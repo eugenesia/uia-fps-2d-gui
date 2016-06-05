ce202c861e7974318d3c58a1049cfc70e9aae720
Eugene Sia <eugene.sia@ticketmaster.co.uk>
2016-06-05 13:09:56 +0100

    Integrate and event system sect 6.4.1

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,7 @@
+﻿// Constants for event messages.
+// Useful for organising event messages so we don't have to type them
+// all over the place.
+public static class GameEvent {
+	public const string ENEMY_HIT = "ENEMY_HIT";
+	public const string SPEED_CHANGED = "SPEED_CHANGED";
+}
