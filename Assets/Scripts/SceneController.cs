f082581fa657d83826f3bb7f054674cf475392bf
Eugene Sia <eugene.sia@ticketmaster.co.uk>
2016-06-06 01:06:07 +0100

    Update cloned enemy speed from UI setting

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -4,7 +4,23 @@ using System.Collections;
 public class SceneController : MonoBehaviour {
 	[SerializeField] private GameObject enemyPrefab;
 	private GameObject _enemy;
-	
+
+	// Add listener to our custom messaging system.
+	void Awake() {
+		Messenger<float>.AddListener(GameEvent.SPEED_CHANGED, OnSpeedChanged);
+	}
+
+	// Clean up listeners on this MonoBehaviour being destroyed.
+	void OnDestroy() {
+		Messenger<float>.RemoveListener(GameEvent.SPEED_CHANGED, OnSpeedChanged);
+	}
+
+	// Event handler.
+	private void OnSpeedChanged(float value) {
+		// On changing speed value in UI, set the new speed for later cloned enemies.
+		enemyPrefab.GetComponent<WanderingAI>().speed = WanderingAI.baseSpeed * value;
+	}
+
 	void Update() {
 		if (_enemy == null) {
 			_enemy = Instantiate(enemyPrefab) as GameObject;
